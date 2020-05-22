19d746a0de880364422ec2b27e4dd01416c132a0
Sam Doser <Sam Sylph@DESKTOP-A94VOTQ>
2020-05-21 20:12:43 -0400

    Added code to draw 30 individually defined polygons and display them.

--- Polar TicTacToe/src/main/Driver.java
+++ Polar TicTacToe/src/main/Driver.java
@@ -2,9 +2,9 @@ package main;
 
 public class Driver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("What is the big deal,");
 	}
 
 }
